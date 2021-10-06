f11a8994ab7587a4566035aa2acced4d43311f0d
Tanise Banda <tanise@malabarba.com.br>
2021-10-06 01:15:14 -0300

    Upload do projeto e correção dos bugs 1, 3, 4 (cliente 1 e 2) e  6 (cliente 1).

---
+++ Desafio-aula-1/registradora-cheia-de-bugs/src/ItensPorQuantidade.java
@@ -0,0 +1,10 @@
+public class ItensPorQuantidade {
+
+    static int pao = 60;
+    static int torta = 64;
+    static int sanduiche = 20;
+    static int leite = 20;
+    static int cafe = 20;
+
+
+}
