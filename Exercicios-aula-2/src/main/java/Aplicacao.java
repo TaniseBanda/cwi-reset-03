db903a5703c626d4ec569ac2cf637f60f867d803
Tanise Banda <tanise@malabarba.com.br>
2021-10-09 11:04:13 -0300

    resolucao exercicio em parte

---
+++ Exercicios-aula-2/src/main/java/Aplicacao.java
@@ -0,0 +1,6 @@
+public class Aplicacao {
+    public static void main(String[] args) {
+        Filme filme = new Filme ("Nasce uma Estrela", "Um dia, um cantor no auge da fama, após deixar uma apresentação, ele para em um bar para beber algo. É quando conhece Ally, uma insegura cantora que ganha a vida trabalhando em um restaurante", 136, 2018, "Bradley Cooper");
+        Filme.reproduzir();
+    }
+}
--- Exercicios-aula-2/src/main/java/Filme.java
+++ Exercicios-aula-2/src/main/java/Filme.java
@@ -1,2 +1,28 @@
 public class Filme {
+     private String nome;
+    private String descricao;
+     private int duração;
+     private int anoLancamento;
+     private String nomeDiretor;
+
+     //construtor
+    public Filme(String nome, String descricao, int duração, int anoLancamento, String nomeDiretor) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.duração = duração;
+        this.anoLancamento = anoLancamento;
+        this.nomeDiretor = nomeDiretor;
+    }
+
+    public void reproduzir() {
+        return System.out.println ("Nome do filme : " + this.nome + "\n" + "Descrição do filme : " + this.descricao + "\n" + "Tempo de duração do filme : " + this.duracao + "\n" + "Nome do diretor do filme : " + this.nomeDiretor);
+    }
+
+
+    //avaliacao
+
+    //dados do diretor
+
+
+
 }
