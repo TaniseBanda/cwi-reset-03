3a11fc53bf5739fb2c9eee9315b2ad88c1d76d57
Tanise Banda <tanise@malabarba.com.br>
2021-10-09 12:24:08 -0300

    exercicio 2 ate onde consegui ir antes da resolução

--- Exercicios-aula-2/src/main/java/Ator.java
+++ Exercicios-aula-2/src/main/java/Ator.java
@@ -1,2 +1,23 @@
-//public class Ator ]{
-//        }
+public class Ator {
+    private String nome;
+    private Integer idade;
+    private Integer numOscars;
+    private String descricao;
+
+    public Ator(String nome, Integer idade, Integer numOscars, String descricao) {
+        this.nome = nome;
+        this.idade = idade;
+        this.numOscars = numOscars;
+        this.descricao = descricao;
+    }
+    public enum Genero {
+        MASCULINO ("Masculino"),
+        FEMININO ("Feminino"),
+        NAO_BINARIO ("Não Binário");
+    }
+
+    public String getGenero() {
+        return descricao;
+    }
+
+}
