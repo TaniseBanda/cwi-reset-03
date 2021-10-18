241d783ca99ae48b3fa9c979325846bc508f30d4
Tanise Banda <tanise@malabarba.com.br>
2021-10-18 15:49:15 -0300

    Desafio parte 2 - Ajuste codigo FakeDatabase e criacao da classe ator controller.

---
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tanisebanda/controller/AtorController.java
@@ -0,0 +1,33 @@
+package br.com.cwi.reset.tanisebanda.controller;
+
+import br.com.cwi.reset.tanisebanda.FakeDatabase;
+import br.com.cwi.reset.tanisebanda.model.Ator;
+import br.com.cwi.reset.tanisebanda.request.AtorRequest;
+import br.com.cwi.reset.tanisebanda.service.AtorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/atores")
+public class AtorController {
+
+    private AtorService atorService;
+
+    public AtorController() {
+        this.atorService = new AtorService(FakeDatabase.getInstance());
+    }
+
+    //demais métodos
+
+    //public List<Ator> consultarAtores() {
+    //   this.atorService.consultarAtores();
+    //}
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void criarAtor(@RequestBody AtorRequest atorRequest) throws Exception {
+        this.atorService.criarAtor(atorRequest);
+    }
+
+}
--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tanisebanda/controller/HelloWorldController.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/tanisebanda/controller/HelloWorldController.java
@@ -1,7 +1,8 @@
-package br.com.cwi.reset.tanisebanda;
+package br.com.cwi.reset.tanisebanda.controller;
 
 import br.com.cwi.reset.tanisebanda.exception.NomeNaoInformadoException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello-world")
 public class HelloWorldController {
     
-    @GetMapping
+    @PostMapping
     public String helloWorld() throws Exception {
         throw new NomeNaoInformadoException();
         //return "Minha API resetflix está UP!!!";
